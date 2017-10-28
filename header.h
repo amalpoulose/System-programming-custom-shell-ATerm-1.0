486f073ffd724440591ea7fa284245f4d2fdf805
Amal Poulose <amalpoulose1995@hotmail.com>
2017-10-28 11:22:40 +0530

    Add files via upload

---
+++ header.h
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+
+
+#define MAX 10
+#define LINE 128
+typedef struct node
+{
+	char *data;
+	int num;
+	struct node *next;
+}QUE;
+
+
+void space_bgn(char *);
+void pipecmd(char *);
+void command(char *);
+void arr_str(char **,char *);
+void command(char *);
+void defaultcmd(char **);
+void enqueue(QUE **,char*,int);
+void dequeue(QUE **);
+
