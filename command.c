3c4aa14905f6360849428d890f9471f8489523a3
Amal Poulose <amalpoulose1995@hotmail.com>
2017-11-03 10:40:51 +0530

    Update command.c

--- command.c
+++ command.c
@@ -9,6 +9,12 @@ void command(char *s)
 
 	if(strcmp(*p,"hist")==0)
 		hist(q);
+	else if(strcmp(*p,"curPid")==0)
+		printf("%d\n",getpid());
+	else if(strcmp(*p,"pPid")==0)
+		printf("%d\n",getppid());
+	else if(strcmp(*p,"version")==0)
+		printf("Aterm 1.0\ncreated by : Amal Poulose\n");
 	else
 		defaultcmd(p);
 
